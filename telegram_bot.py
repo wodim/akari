df43c302f3190a6462c804f42b36468509c51573
wodim <neikokz@gmail.com>
2016-06-20 13:51:12 +0000

    telegram bot: send "sending photo" notification

--- telegram_bot.py
+++ telegram_bot.py
@@ -5,6 +5,7 @@ import telepot.async
 
 from akari import akari_search
 from config import config
+import utils
 
 
 class BotException(Exception):
@@ -24,6 +25,13 @@ class AkariBot(telepot.async.Bot):
             if content_type != 'text':
                 return
 
+            utils.logging.info(('Message from {chat_id} ({type}): ' +
+                                '"{message_text}"')
+                               .format(chat_id=chat_id,
+                                       type=message['chat']['type'],
+                                       message_text=message['text']))
+
+            await self.sendChatAction(chat_id, 'upload_photo')
             filename, caption = self._process_chat_message(message['text'])
             await self._send_reply(message, caption, filename=filename)
         except Exception as e:
@@ -32,17 +40,22 @@ class AkariBot(telepot.async.Bot):
             except:
                 exception_string = '???'
             finally:
+                utils.logging.info(('Error sent to {chat_id} ({type}): ' +
+                                    '{exception}')
+                                   .format(chat_id=chat_id,
+                                           type=message['chat']['type'],
+                                           exception=exception_string))
                 error_message = 'Error: ' + exception_string
                 await self._send_reply(message, error_message)
 
-    async def _send_reply(self, message, reply, filename=None):
+    async def _send_reply(self, message, caption, filename=None):
         if filename:
             f = open(filename, 'rb')
             await self.sendPhoto(message['chat']['id'],
-                                 f, caption=reply)
+                                 f, caption=caption)
         else:
             await self.sendMessage(message['chat']['id'],
-                                   reply)
+                                   caption)
 
     def _process_chat_message(self, text):
         try:
