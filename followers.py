1e5bee7c4183aa48bb08414b0c9ff24167c7541b
wodim <neikokz@gmail.com>
2016-07-14 18:13:46 +0000

    unfollow those who have unfollowed me

--- followers.py
+++ followers.py
@@ -4,6 +4,16 @@ from twitter import twitter
 
 
 def follow_my_followers():
-    for user in tweepy.Cursor(twitter.api.followers).items(100):
-        if not user.following:
-            user.follow()
+    for page in tweepy.Cursor(twitter.api.followers, count=200).pages():
+        for user in page:
+            if not user.following:
+                user.follow()
+
+
+def unfollow_my_unfollowers():
+    # 100 per page is the max lookup_friendships can do
+    for page in tweepy.Cursor(twitter.api.friends, count=100).pages():
+        user_ids = [user.id for user in page]
+        for user in twitter.api._lookup_friendships(user_ids):
+            if not user.is_followed_by:
+                twitter.api.destroy_friendship(user.id)
